7cb72e6f1a218768c17760f3a2d6f85dc01ac050
ajneu <ajneu@users.noreply.github.com>
2016-03-26 09:01:16 +0100

    str_const for c++14

--- str_const/str_const.cpp
+++ str_const/str_const.cpp
@@ -9,7 +9,6 @@ constexpr bool c_str_same(char const* str_a, char const* str_b)
             : false);
 }
 
-
 // code on page 29 https://github.com/boostcon/cppnow_presentations_2012/blob/master/wed/schurr_cpp11_tools_for_class_authors.pdf?raw=true
 class str_const {                                // constexpr string
 private:
@@ -21,20 +20,46 @@ public:
         p_(a), sz_(N-1) {}
     constexpr str_const(const str_const &str, std::size_t offset) :
         p_(str.p_+offset), sz_(str.sz_-offset) {}
-    constexpr char operator[](std::size_t n) {   // []
+    constexpr char operator[](std::size_t n) const {   // []
         return n < sz_ ? p_[n] :
             throw std::out_of_range("");
     }
-    constexpr std::size_t size() { return sz_; } // size()
-    constexpr bool eq(const str_const &rhs) {     // the following conflicts: constexpr bool operator==(const str_const &rhs) {
+    constexpr std::size_t size() const { return sz_; } // size()
+    constexpr bool eq(const str_const &rhs) const {     // the following conflicts: constexpr bool operator==(const str_const &rhs) {
         return c_str_same(p_, rhs.p_);
     }
 
-    constexpr operator const char*() {
+   constexpr operator const char*() const {
         return p_;
     }
 
 };
+
+
+
+
+
+template <std::size_t N>
+struct str_c {
+   constexpr str_c(const char(&s)[N])
+      : str{s}
+   {
+   }
+   const char (&str)[N];
+   static constexpr std::size_t size = N-1;
+   using Ret = const char(&)[N];
+   constexpr operator Ret() const {
+        return str;
+    }
+
+};
+
+template <std::size_t N>
+constexpr str_c<N> make_str_c(const char(&s)[N])
+{
+   return str_c<N>(s);
+}
+
 
 int main()
 {
@@ -48,8 +73,10 @@ int main()
     static_assert(world.eq("world!"), "");
     std::cout << world << std::endl;
 
-
-    // // this ain't working ...
     //constexpr str_const world(my_string, 7, 5);
     //    static_assert(world == "world", "");
+
+
+    constexpr auto s = make_str_c("asdf");
+    std::cout << s << std::endl;
 }
